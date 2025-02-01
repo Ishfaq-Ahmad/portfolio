7a563f1b324773cafed849f71c574a89bdf037c1
Ishfaq Ahmad <ishfaq9701@gmail.com>
2025-02-01 16:40:00 +0500

    testing

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -97,7 +97,7 @@
             <img src="{{ asset('assets/img/hero-bg.jpg')}}" alt="" data-aos="fade-in" class="">
 
             <div class="container" data-aos="fade-up" data-aos-delay="100">
-                <h2>Ishfaq Ahmad khan</h2>
+                <h2>Ishfaq Ahmad</h2>
                 <p>I am <span class="typed" data-typed-items="Developer, Freelancer">Developer</span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
 
             </div>
